c5a28a8904ef40bb8392d8a4d65f6ae7dee990a3
Anolue Ekene Samuel <kenelight4u@gmail.com>
2021-10-10 00:33:13 +0100

    Update

---
+++ Interfaces/IAccount.cs
@@ -0,0 +1,33 @@
+using CreditVillageBackend.ViewModels.RequestViewModels;
+using CreditVillageBackend.ViewModels.ResponseViewModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CreditVillageBackend
+{
+    public interface IAccount
+    {
+        Task<RegisterResponse> UserRegisterAsync(RegisterRequest userRequest);
+
+        Task<RegisterResponse> AdminRegisterAsync(RegisterRequest userRequest);
+
+        Task<ResendCodeResponse> UserResendCodeAsync(ResendCodeRequest userRequest);
+
+        Task<VerifyResponse> UserVerifyAsync(VerifyRequest userRequest);
+
+        Task<LoginResponse> LoginAsync(LoginRequest userRequest);
+
+        Task<ForgetPasswordResponse> UserForgetPasswordAsync(ForgetPasswordRequest userRequest);
+
+        Task<ChangePasswordResponse> ChangePasswordAsync(string GetUserId, ChangePasswordRequest changePasswordRequest);
+
+        Task<GetUserResponse> GetUserAsync(string UserId);
+
+        Task<UpdateResponse> UpdateUserAsync(string GetUserId, UpdateRequest updateRequest);
+
+        Task<EditResponse> EditUserDetailsAsync(string GetUserId, EditRequest editRequest);
+
+        Task<ResetPasswordResponse> ResetPasswordAsync(ResetPasswordRequest userRequest);
+    }
+}
---
+++ ViewModels/ResponseViewModels/VerifyResponse.cs
@@ -0,0 +1,11 @@
+namespace CreditVillageBackend
+{
+    public class VerifyResponse
+    {
+        public bool Check { get; set; }
+
+        public string Status { get; set; }
+
+        public string Message { get; set; }
+    }
+}
